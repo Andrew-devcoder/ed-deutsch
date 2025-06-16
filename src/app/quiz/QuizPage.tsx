78a05ee9bc0d62f0ce9735ce58d92d5e7ee4941c
Andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-06-16 10:29:59 +0200

    test deploy

---
+++ src/app/quiz/QuizPage.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { useSearchParams, useRouter } from 'next/navigation';
+import { verbs } from '@/data/verbs';
+import { useEffect, useState } from 'react';
+
+export default function QuizPage() {
+	const searchParams = useSearchParams();
+	const router = useRouter();
+	const id = parseInt(searchParams.get('id') || '1', 10);
+	const correct = parseInt(searchParams.get('correct') || '0', 10);
+	const question = verbs[id - 1];
+	const [selected, setSelected] = useState<string | null>(null);
+	const [shuffledOptions, setShuffledOptions] = useState<string[]>([]);
+
+	useEffect(() => {
+		if (!question || !selected) return;
+
+		const isCorrect = selected === question.correct;
+
+		const timeout = setTimeout(() => {
+			if (id >= verbs.length) {
+				router.push(`/result?correct=${isCorrect ? correct + 1 : correct}&total=${verbs.length}`);
+			} else {
+				router.push(`?id=${id + 1}&correct=${isCorrect ? correct + 1 : correct}`);
+			}
+		}, 1000);
+
+		return () => clearTimeout(timeout);
+	}, [selected, question, id, correct, router]);
+
+	function shuffle<T>(array: T[]): T[] {
+		return [...array].sort(() => Math.random() - 0.5);
+	}
+
+	useEffect(() => {
+		setSelected(null);
+		if (question) {
+			setShuffledOptions(shuffle(question.options));
+		}
+	}, [id]);
+
+	if (!question) {
+		return (
+			<div className="p-4">
+				<h1>Питання не знайдено</h1>
+				<button onClick={() => router.push('/')}>На головну</button>
+			</div>
+		);
+	}
+
+	return (
+		<div className="p-4">
+			<h2 className="text-xl mb-4">
+				Partizip II для: <b>{question.infinitiv}</b>
+			</h2>
+			<div className="grid gap-2 grid-cols-2">
+				{shuffledOptions.map((option) => {
+					const isCorrect = option === question.correct;
+					const isSelected = selected === option;
+
+					let buttonStyle = 'border px-4 py-2 rounded hover:bg-blue-100';
+
+					if (selected) {
+						if (isCorrect) buttonStyle += ' bg-green-300';
+						else if (isSelected) buttonStyle += ' bg-red-300 opacity-60';
+						else buttonStyle += ' opacity-60';
+					}
+
+					return (
+						<button
+							key={option}
+							onClick={() => {
+								if (!selected) setSelected(option);
+							}}
+							className={buttonStyle}
+							disabled={!!selected}
+						>
+							{option}
+						</button>
+					);
+				})}
+			</div>
+
+			{selected && selected !== question.correct && (
+				<div className="mt-4 mx-auto w-fit">
+					<button
+						onClick={() => router.push(`?id=${id + 1}`)}
+						className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+					>
+						Далі
+					</button>
+				</div>
+			)}
+		</div>
+	);
+}
